13a5da64ff4e9e7ad9a4339998bd466c5bfa74be
Linh B Ngo <lngo@wcupa.edu>
2022-04-22 11:35:13 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -13,9 +13,9 @@ This profile provides the template to launch a Kubernetes cluster.
 pc.defineParameter( "n", "Number of kubelet (4 or more)", portal.ParameterType.INTEGER, 4 )
 params = pc.bindParameters()
 
-if params.n < 5:
-  portal.context.reportError( portal.ParameterError( "You must request at least 4 kubelet nodes." ) )
-  
+# Create a Request object to start building the RSpec.
+request = pc.makeRequestRSpec()
+
 #
 # Setup the Tour info with the above description and instructions.
 #  
