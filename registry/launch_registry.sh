65c9e50cd4ff076e1fe85436fc8f4970f04f7051
Linh B Ngo <lngo@wcupa.edu>
2022-04-24 09:39:30 -0400

    Update launch_registry.sh

--- registry/launch_registry.sh
+++ registry/launch_registry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+sleep 120
 sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
 sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=5000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
 sleep 90
