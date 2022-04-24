701c0ace5f1ca7939cde06d4002dc73c81a3fca1
Linh B Ngo <lngo@wcupa.edu>
2022-04-24 00:01:48 -0400

    Update build_jenkins.sh

--- jenkins/build_jenkins.sh
+++ jenkins/build_jenkins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd /local/repository/jenkins
-sed -i "s/KUBEHEAD/$(cat /opt/keys/headnode)/g" /local/repository/jenkins/casc.yaml
+ip_address=$(kubectl get nodes -o wide | grep master | awk '{print $6}')
+sed -i "s/KUBEHEAD/${ip_address}/g" /local/repository/jenkins/casc.yaml
 docker build -t $(cat /opt/keys/headnode):5000/jenkins:jcasc .
 docker push $(cat /opt/keys/headnode):5000/jenkins:jcasc
 
