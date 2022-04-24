495eea3b431a681c7b8ddecb277440ceea064903
Linh B Ngo <lngo@wcupa.edu>
2022-04-23 23:06:20 -0400

    Update build_jenkins.sh

--- jenkins/build_jenkins.sh
+++ jenkins/build_jenkins.sh
@@ -2,4 +2,6 @@
 
 cd /local/repository/jenkins
 sed -i "s/KUBEHEAD/$(cat /opt/keys/headnode)/g" /local/repository/jenkins/casc.yaml
-docker build -t jenkins:jcasc .
+docker build -t KUBEHEAD:5000/jenkins:jcasc .
+docker push KUBEHEAD:5000/jenkins:jcasc
+
