381ef938ef26a1c2c079d5066340f433e32a49eb
Linh B Ngo <lngo@wcupa.edu>
2022-04-24 00:01:14 -0400

    Update launch_jenkins.sh

--- jenkins/launch_jenkins.sh
+++ jenkins/launch_jenkins.sh
@@ -5,7 +5,6 @@ kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin
 kubectl -n kube-system create sa jenkins
 kubectl create clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:jenkins
 
-ip_address=$(kubectl get nodes -o wide | grep master | awk '{print $6}')
-sed -i "s/KUBEHEAD/${ip_address}/g" /local/repository/jenkins/jenkins.yaml
+sed -i "s/KUBEHEAD/$(cat /opt/keys/headnode)/g" /local/repository/jenkins/jenkins.yaml
 kubectl create -f /local/repository/jenkins/jenkins.yaml --namespace jenkins
 kubectl create -f /local/repository/jenkins/jenkins-service.yaml --namespace jenkins
