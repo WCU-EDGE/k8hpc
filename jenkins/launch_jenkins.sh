1278e42713988c7057181584b0decf1b0f98d874
Linh B Ngo <lngo@wcupa.edu>
2022-04-28 15:27:23 -0400

    Update launch_jenkins.sh

--- jenkins/launch_jenkins.sh
+++ jenkins/launch_jenkins.sh
@@ -6,8 +6,9 @@ kubectl -n kube-system create sa jenkins
 kubectl create clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:jenkins
 
 sed -i "s/KUBEHEAD/$(cat /opt/keys/headnode)/g" /local/repository/jenkins/jenkins-values.yaml
-chart=jenkinsci/jenkins
-helm install jenkins -n jenkins -f jenkins-values.yaml $chart
+
+helm repo add jenkinsci https://charts.jenkins.io
+helm install jenkins -n jenkins -f jenkins-values.yaml jenkinsci/jenkins
 
 
 #kubectl create -f /local/repository/jenkins/jenkins.yaml --namespace jenkins
