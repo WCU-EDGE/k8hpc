73b5e4106563496921cec79c5dea4347fcc698a0
Linh B Ngo <lngo@wcupa.edu>
2023-02-23 21:04:33 -0500

    Update kube_manager.sh

--- kube_manager.sh
+++ kube_manager.sh
@@ -21,9 +21,11 @@ done
 
 sudo systemctl restart nfs-kernel-server
 
-kubeadm init > /opt/keys/kube.log
+kubeadm init --pod-network-cidr=10.244.0.0/16 > /opt/keys/kube.log
+
 sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
 sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=5000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
+
 sleep 90
 
 sudo touch /opt/keys/kube_done
@@ -36,11 +38,6 @@ while IFS= read -r line; do
   sudo chown $line: /users/$line/.kube/config
 done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
 
-sudo -H -u lngo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')" 
+sudo -H -u $1 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
-# set up helm
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod +x get_helm.sh
-./get_helm.sh
-rm get_helm.sh
 
